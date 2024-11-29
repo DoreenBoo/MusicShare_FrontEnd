553c66f67cb52a0d0359957cb7079931eb0111eb
DODO <1317025828@qq.com>
2024-11-29 10:05:03 +0800

    初始化home页面，添加ant依赖，修复一些bug

--- MusicShare/src/components/HelloOFY.vue
+++ MusicShare/src/components/HelloOFY.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="hello" >
+  <div>
     <h1>{{ title }}</h1>
     <!-- 自定义按钮颜色 -->
        <!-- 使用 router-link 跳转 -->
-      <router-link to="/login">
-        <a-button class="custom-button">GetStart</a-button>
-      </router-link>
+   <div class="button-container">
+    <router-link to="/login">
+      <a-button class="custom-button">GetStart</a-button>
+    </router-link>
+  </div>
     
   </div>
 </template>
@@ -18,31 +20,31 @@ export default {
   },
 
 }
-</script>
-
+</script >
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style >
 h1{
   color:rgb(254, 67, 101);
    font-size: 100px;
    font-family: 'melete';
     font-weight: 300;/* 字体粗细 */
-       transform: scaleY(1.5); /* 上下拉长字体，1.5 为拉伸倍数 */
-      transform: translateY(150px); /* 向下移动 100px */
-    
 }
+.button-container {
+  display: flex;           /* 使用 Flexbox 布局 */
+  justify-content: center; /* 水平居中 */
+  align-items: center;     /* 垂直居中 */
 
+}
 
 .custom-button {
   background-color: rgb(249, 205, 173); /* 默认背景颜色 */
   color: white;
   border-color: rgb(255,255,255); /* 默认边框颜色 */
   transition: background-color 0.3s, border-color 0.3s; /* 添加平滑过渡效果 */
-  font-size: 60px; /* 设置字体大小 */
- width: 300px; /* 设置固定宽度 */
-  height: 100px; /* 设置固定高度 */
-   transform: translateY(200px); 
-    border-radius: 40px; /* 设置圆角，20px为圆角的半径，可以根据需要调整 */
+  font-size: 60px; 
+  width: 300px; 
+  height: 100px;
+  border-radius: 40px; /* 设置圆角，20px为圆角的半径，可以根据需要调整 */
 
 }
 
@@ -52,18 +54,4 @@ h1{
   border-color: rgb(254, 67, 101); /* 悬停时改变边框颜色 */
 }
 
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: rgb(254, 67, 101);
-}
 </style>
--- MusicShare/src/main.js
+++ MusicShare/src/main.js
@@ -3,13 +3,19 @@ import App from './App.vue';
 
 import router from './router/index';  // 引入路由配置
 
-import "@/assets/text/text.css"; // 引入你的 CSS 文件
+import "@/assets/text/text.css"; 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+
 
 const app = createApp(App);
 // 使用 Vue Router
 app.use(router);
 
 // 挂载到 DOM
+// app.mount('#app');
+
+app.use(Antd);  // 注册 Ant Design Vue
 app.mount('#app');
 
 
