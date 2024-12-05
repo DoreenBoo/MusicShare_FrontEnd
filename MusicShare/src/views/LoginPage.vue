6c64912632ae8c524ad13de4ab2838ba49a043e4
DODO <1317025828@qq.com>
2024-12-05 13:44:33 +0800

    修改登录注册页面

--- MusicShare/src/views/LoginPage.vue
+++ MusicShare/src/views/LoginPage.vue
@@ -1,75 +1,251 @@
 <template>
-    <!-- 登录/注册表单 -->
-        <div class="login-wrapper">
-            <div class="header">Login</div>
-            <div class="form-wrapper">
-                <input type="text" name="username" placeholder="username" class="input-item">
-                <input type="password" name="password" placeholder="password" class="input-item">
-                <button class="btn">Login</button>
-            </div>
-            <div class="msg">
-                Don't have account?
-                <router-link to="/register">Sign up</router-link>
-            </div>
+  <!-- 登录/注册表单 -->
+  <div class="login-wrapper">
+    <div class="header">Login</div>
+    <div class="form-wrapper">
+      <!-- 选择登录方式 -->
+      <div class="login-option">
+        <button
+          class="login-method-btn"
+          :class="{'active': loginMethod === 'username'}"
+          @click="loginMethod = 'username'"
+        >
+          Password
+        </button>
+        <button
+          class="login-method-btn"
+          :class="{'active': loginMethod === 'phone'}"
+          @click="loginMethod = 'phone'"
+        >
+          Phone
+        </button>
+      </div>
+
+      <!-- 账号密码登录 -->
+      <div v-if="loginMethod === 'username'">
+        <input
+          type="text"
+          name="username"
+          v-model="username"
+          placeholder="username"
+          class="input-item"
+        />
+        <input
+          type="password"
+          name="password"
+          v-model="password"
+          placeholder="password"
+          class="input-item"
+        />
+      </div>
+
+      <!-- 手机号验证码登录 -->
+      <div v-if="loginMethod === 'phone'">
+        <input
+          type="text"
+          name="phone"
+          v-model="phone"
+          placeholder="phone"
+          class="input-item"
+        />
+        <div class="verification-code-wrapper">
+          <input
+            type="text"
+            v-model="verificationCode"
+            placeholder="verification code"
+            class="input-item"
+          />
+          <button
+            class="code-btn"
+            :disabled="isCodeSending || countdown > 0"
+            @click="sendVerificationCode"
+          >
+            {{ countdown > 0 ? `${countdown}s` : 'Get Code' }}
+          </button>
         </div>
-   
+      </div>
+
+      <div v-if="loginError" class="error-message">Invalid credentials</div>
+
+      <button class="btn" @click="handleLogin">Login</button>
+    </div>
+
+    <div class="msg">
+      Don't have an account?
+      <router-link to="/register">Sign up</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginPage',
+  data() {
+    return {
+      loginMethod: 'username', // 默认登录方式：'username' 或 'phone'
+      username: '',
+      password: '',
+      phone: '',
+      verificationCode: '',
+      generatedCode: '', // 存储生成的验证码
+      countdown: 0, // 倒计时
+      loginError: false, // 登录错误消息
+      isCodeSending: false, // 是否正在发送验证码
+    };
+  },
+  methods: {
+    // 模拟发送验证码
+    sendVerificationCode() {
+      if (!this.phone) {
+        alert('Please enter a valid phone number');
+        return;
+      }
+      this.isCodeSending = true;
+      this.generatedCode = Math.floor(100000 + Math.random() * 900000).toString(); // 随机6位验证码
+      console.log('Generated Code:', this.generatedCode); // 控制台输出验证码
+      setTimeout(() => {
+        alert('Verification code sent to your phone!');
+        this.isCodeSending = false;
+        this.startCountdown();
+      }, 1000); // 模拟网络请求延迟
+    },
+    // 启动倒计时
+    startCountdown() {
+      this.countdown = 60;
+      const interval = setInterval(() => {
+        this.countdown--;
+        if (this.countdown <= 0) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    },
+    handleLogin() {
+      // 如果选择了用户名密码登录
+      if (this.loginMethod === 'username') {
+        if (!this.username || !this.password) {
+          this.loginError = true;
+          return;
+        }
+        // 模拟登录操作
+        console.log('Logging in with username and password...', {
+          username: this.username,
+          password: this.password,
+        });
+        this.loginError = false;
+      }
+      // 如果选择了手机号验证码登录
+      else if (this.loginMethod === 'phone') {
+        if (!this.phone || !this.verificationCode || this.verificationCode !== this.generatedCode) {
+          this.loginError = true;
+          return;
+        }
+        // 模拟登录操作
+        console.log('Logging in with phone and verification code...', {
+          phone: this.phone,
+          verificationCode: this.verificationCode,
+        });
+        this.loginError = false;
+      }
+    },
+  },
 };
 </script>
 
 <style>
-
-
 /* 登录框样式 */
 .login-wrapper {
   background-color: rgb(249, 205, 173);
   width: 358px;
-  height: 588px;
+  height: 650px;
   border-radius: 40px;
   padding: 0 50px;
 }
 
+.header {
+  font-size: 38px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 200px;
+}
 
- .header {
-     font-size: 38px;
-     font-weight: bold;
-     text-align: center;
-     line-height: 200px;
- }
+.input-item {
+  display: block;
+  width: 100%;
+  margin-bottom: 20px;
+  border: 0;
+  padding: 10px;
+  border-bottom: 1px solid rgb(128, 125, 125);
+  font-size: 15px;
+  outline: none;
+  border-radius: 40px;
+}
 
- .input-item {
-     display: block;
-     width: 100%;
-     margin-bottom: 20px;
-     border: 0;
-     padding: 10px;
-     border-bottom: 1px solid rgb(128, 125, 125);
-     font-size: 15px;
-     outline: none;
-      border-radius: 40px;
- }
+.verification-code-wrapper {
+  display: flex;
+  align-items: center;
+}
 
- .input-item:placeholder {
-     text-transform: uppercase;
- }
+.code-btn {
+  background-color: rgb(254, 67, 101);
+  color: #fff;
+  border: none;
+  border-radius: 20px;
+  margin-left: 10px;
+  padding: 5px 15px;
+  cursor: pointer;
+  font-size: 12px;
+}
 
- .btn {
-     text-align: center;
-     padding: 10px;
-     width: 100%;
-     margin-top: 40px;
-     background-color:  rgb(254, 67, 101); 
-       border-color: rgb(254, 67, 101) ;
-     color: #fff;
-      border-radius: 40px;
- }
+.code-btn:disabled {
+  background-color: gray;
+  cursor: not-allowed;
+}
 
- .msg {
-     text-align: center;
-     line-height: 88px;
- }
+.login-option {
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.login-method-btn {
+  padding: 10px 30px;
+  background-color: rgb(254, 67, 101);
+  color: white;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 14px;
+  width: 48%;
+}
+
+.login-method-btn.active {
+  background-color: #ff3e5b;
+}
+
+.btn {
+  text-align: center;
+  padding: 10px;
+  width: 100%;
+  margin-top: 40px;
+  background-color: rgb(254, 67, 101);
+  border-color: rgb(254, 67, 101);
+  color: #fff;
+  border-radius: 40px;
+}
+
+.msg {
+  text-align: center;
+  line-height: 88px;
+}
+
+a {
+  text-decoration-line: none;
+  color: #abc1ee;
+}
+
+.error-message {
+  color: red;
+  text-align: center;
+  margin-bottom: 10px;
+}
 </style>
--- MusicShare/src/views/RegisterPage.vue
+++ MusicShare/src/views/RegisterPage.vue
@@ -3,10 +3,51 @@
   <div class="register-wrapper">
     <div class="header">Register</div>
     <div class="form-wrapper">
-      <input type="text" name="username" v-model="username" placeholder="username" class="input-item" />
-      <input type="password" name="password" v-model="password" placeholder="password" class="input-item" />
-      <input type="password" name="confirmPassword" v-model="confirmPassword" placeholder="confirm password" class="input-item" />
+      <input 
+        type="number" 
+        name="phone" 
+        v-model="phone" 
+        placeholder="phone" 
+        class="input-item" 
+      />
+      <div class="verification-code-wrapper">
+        <input 
+          type="text" 
+          v-model="verificationCode" 
+          placeholder="verification code" 
+          class="input-item" 
+        />
+        <button 
+          class="code-btn" 
+          :disabled="isCodeSending || countdown > 0" 
+          @click="sendVerificationCode"
+        >
+          {{ countdown > 0 ? `${countdown}s` : 'Get Code' }}
+        </button>
+      </div>
+      <input 
+        type="text" 
+        name="username" 
+        v-model="username" 
+        placeholder="username" 
+        class="input-item" 
+      />
+      <input 
+        type="password" 
+        name="password" 
+        v-model="password" 
+        placeholder="password" 
+        class="input-item" 
+      />
+      <input 
+        type="password" 
+        name="confirmPassword" 
+        v-model="confirmPassword" 
+        placeholder="confirm password" 
+        class="input-item" 
+      />
       <div v-if="passwordMismatch" class="error-message">Passwords do not match</div>
+      <div v-if="codeError" class="error-message">Invalid verification code</div>
       <button class="btn" @click="handleRegister">Register</button>
     </div>
     <div class="msg">
@@ -20,25 +61,65 @@
 export default {
   data() {
     return {
+      phone: '',
+      verificationCode: '',
+      generatedCode: '', // 存储生成的验证码
       username: '',
       password: '',
       confirmPassword: '',
-      passwordMismatch: false, // 用于控制错误消息的显示
+      passwordMismatch: false, // 控制密码错误消息
+      codeError: false, // 控制验证码错误消息
+      isCodeSending: false, // 控制是否在发送验证码
+      countdown: 0, // 倒计时
     };
   },
   methods: {
+    // 模拟发送验证码
+    sendVerificationCode() {
+      if (!this.phone) {
+        alert("Please enter a valid phone number");
+        return;
+      }
+      this.isCodeSending = true;
+      this.generatedCode = Math.floor(100000 + Math.random() * 900000).toString(); // 随机6位验证码
+      console.log("Generated Code:", this.generatedCode); // 控制台输出验证码
+      setTimeout(() => {
+        alert("Verification code sent to your phone!");
+        this.isCodeSending = false;
+        this.startCountdown();
+      }, 1000); // 模拟网络请求延迟
+    },
+    // 启动倒计时
+    startCountdown() {
+      this.countdown = 60;
+      const interval = setInterval(() => {
+        this.countdown--;
+        if (this.countdown <= 0) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    },
     handleRegister() {
-      // 检查密码是否相同
+      // 检查密码是否一致
       if (this.password !== this.confirmPassword) {
         this.passwordMismatch = true;
+        return;
       } else {
         this.passwordMismatch = false;
-        // 这里可以执行注册操作，例如发送请求到后台
-        console.log('Registering...', {
-          username: this.username,
-          password: this.password,
-        });
       }
+      // 检查验证码是否正确
+      if (this.verificationCode !== this.generatedCode) {
+        this.codeError = true;
+        return;
+      } else {
+        this.codeError = false;
+      }
+      // 这里可以执行注册操作，例如发送请求到后台
+      console.log("Registering...", {
+        phone: this.phone,
+        username: this.username,
+        password: this.password,
+      });
     },
   },
 };
@@ -48,7 +129,7 @@ export default {
 .register-wrapper {
   background-color: rgb(249, 205, 173);
   width: 358px;
-  height: 588px;
+  height: 650px;
   border-radius: 40px;
   padding: 0 50px;
   margin: 0 auto;
@@ -71,6 +152,27 @@ export default {
   font-size: 15px;
   outline: none;
   border-radius: 40px;
+}
+
+.verification-code-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.code-btn {
+  background-color: rgb(254, 67, 101);
+  color: #fff;
+  border: none;
+  border-radius: 20px;
+  margin-left: 10px;
+  padding: 5px 15px;
+  cursor: pointer;
+  font-size: 12px;
+}
+
+.code-btn:disabled {
+  background-color: gray;
+  cursor: not-allowed;
 }
 
 .input-item::placeholder {
